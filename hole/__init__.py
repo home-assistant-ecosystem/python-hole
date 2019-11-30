25bae56b52f894d547bf553900f6bd186cb515b4
Fabian Affolter <fabian@affolter-engineering.ch>
2019-11-30 21:05:28 +0530

    Use black for formatting

--- hole/__init__.py
+++ hole/__init__.py
@@ -9,26 +9,35 @@ import async_timeout
 from . import exceptions
 
 _LOGGER = logging.getLogger(__name__)
-_INSTANCE = '{schema}://{host}/{location}/api.php'
+_INSTANCE = "{schema}://{host}/{location}/api.php"
 
 
 class Hole(object):
     """A class for handling connections with a *hole instance."""
 
-    def __init__(self, host, loop, session, location='admin', tls=False,
-                 verify_tls=True, api_token=None):
+    def __init__(
+        self,
+        host,
+        loop,
+        session,
+        location="admin",
+        tls=False,
+        verify_tls=True,
+        api_token=None,
+    ):
         """Initialize the connection to a *hole instance."""
         self._loop = loop
         self._session = session
         self.tls = tls
         self.verify_tls = verify_tls
-        self.schema = 'https' if self.tls else 'http'
+        self.schema = "https" if self.tls else "http"
         self.host = host
         self.location = location
         self.api_token = api_token
         self.data = {}
         self.base_url = _INSTANCE.format(
-            schema=self.schema, host=self.host, location=self.location)
+            schema=self.schema, host=self.host, location=self.location
+        )
 
     async def get_data(self):
         """Get details of a *hole instance."""
@@ -36,8 +45,7 @@ class Hole(object):
             async with async_timeout.timeout(5, loop=self._loop):
                 response = await self._session.get(self.base_url)
 
-            _LOGGER.info(
-                "Response from *hole: %s", response.status)
+            _LOGGER.info("Response from *hole: %s", response.status)
             self.data = await response.json()
             _LOGGER.debug(self.data)
 
@@ -51,11 +59,9 @@ class Hole(object):
         params = "versions"
         try:
             async with async_timeout.timeout(5, loop=self._loop):
-                response = await self._session.get(self.base_url,
-                                                   params=params)
+                response = await self._session.get(self.base_url, params=params)
 
-            _LOGGER.info(
-                "Response from *hole: %s", response.status)
+            _LOGGER.info("Response from *hole: %s", response.status)
             self.data = await response.json()
             _LOGGER.debug(self.data)
 
@@ -72,11 +78,9 @@ class Hole(object):
         params = "enable=True&auth={}".format(self.api_token)
         try:
             async with async_timeout.timeout(5, loop=self._loop):
-                response = await self._session.get(self.base_url,
-                                                   params=params)
+                response = await self._session.get(self.base_url, params=params)
 
-            _LOGGER.info(
-                "Response from *hole: %s", response.status)
+            _LOGGER.info("Response from *hole: %s", response.status)
             data = await response.json()
             _LOGGER.debug(data)
 
@@ -84,7 +88,6 @@ class Hole(object):
             msg = "Can not load data from *hole: {}".format(self.host)
             _LOGGER.error(msg)
             raise exceptions.HoleConnectionError(msg)
-
 
     async def disable(self, duration=True):
         """Disable DNS blocking on a *hole instance."""
@@ -94,11 +97,9 @@ class Hole(object):
         params = "disable={}&auth={}".format(duration, self.api_token)
         try:
             async with async_timeout.timeout(5, loop=self._loop):
-                response = await self._session.get(self.base_url,
-                                                   params=params)
+                response = await self._session.get(self.base_url, params=params)
 
-            _LOGGER.info(
-                "Response from *hole: %s", response.status)
+            _LOGGER.info("Response from *hole: %s", response.status)
             data = await response.json()
             _LOGGER.debug(data)
 
@@ -110,49 +111,49 @@ class Hole(object):
     @property
     def status(self):
         """Return the status of the *hole instance."""
-        return self.data['status']
+        return self.data["status"]
 
     @property
     def unique_clients(self):
         """Return the unique clients of the *hole instance."""
-        return self.data['unique_clients']
+        return self.data["unique_clients"]
 
     @property
     def unique_domains(self):
         """Return the unique domains of the *hole instance."""
-        return self.data['unique_domains']
+        return self.data["unique_domains"]
 
     @property
     def ads_blocked_today(self):
         """Return the ads blocked today of the *hole instance."""
-        return self.data['ads_blocked_today']
+        return self.data["ads_blocked_today"]
 
     @property
     def ads_percentage_today(self):
         """Return the ads percentage today of the *hole instance."""
-        return self.data['ads_percentage_today']
+        return self.data["ads_percentage_today"]
 
     @property
     def clients_ever_seen(self):
         """Return the clients_ever_seen of the *hole instance."""
-        return self.data['clients_ever_seen']
+        return self.data["clients_ever_seen"]
 
     @property
     def dns_queries_today(self):
         """Return the dns queries today of the *hole instance."""
-        return self.data['dns_queries_today']
+        return self.data["dns_queries_today"]
 
     @property
     def domains_being_blocked(self):
         """Return the domains being blocked of the *hole instance."""
-        return self.data['domains_being_blocked']
+        return self.data["domains_being_blocked"]
 
     @property
     def queries_cached(self):
         """Return the queries cached of the *hole instance."""
-        return self.data['queries_cached']
+        return self.data["queries_cached"]
 
     @property
     def queries_forwarded(self):
         """Return the queries forwarded of the *hole instance."""
-        return self.data['queries_forwarded']
+        return self.data["queries_forwarded"]
