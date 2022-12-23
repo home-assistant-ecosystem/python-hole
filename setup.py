ff5f61a40d654e999821426916a67d14a4b87ede
Fabian Affolter <fabian@affolter-engineering.ch>
2022-12-23 21:50:59 +0100

    Fix formatting

--- setup.py
+++ setup.py
@@ -11,36 +11,36 @@ except ImportError:
 here = os.path.abspath(os.path.dirname(__file__))
 
 # Get the long description from the relevant file
-with open(os.path.join(here, 'README.rst'), encoding='utf-8') as desc:
+with open(os.path.join(here, "README.rst"), encoding="utf-8") as desc:
     long_description = desc.read()
 
 setup(
-    name='hole',
-    version='0.7.0',
-    description='Python API for interacting with *hole.',
+    name="hole",
+    version="0.7.0",
+    description="Python API for interacting with *hole.",
     long_description=long_description,
-    url='https://github.com/home-assistant-ecosystem/python-hole',
-    download_url='https://github.com/home-assistant-ecosystem/python-hole/releases',
-    author='Fabian Affolter',
-    author_email='fabian@affolter-engineering.ch',
-    license='MIT',
+    url="https://github.com/home-assistant-ecosystem/python-hole",
+    download_url="https://github.com/home-assistant-ecosystem/python-hole/releases",
+    author="Fabian Affolter",
+    author_email="fabian@affolter-engineering.ch",
+    license="MIT",
     install_requires=[
-        'aiohttp<4',
-        'async_timeout>4,<5',
+        "aiohttp<4",
+        "async_timeout>4,<5",
     ],
-    packages=['hole'],
-    python_requires='>=3.8',
+    packages=["hole"],
+    python_requires=">=3.8",
     zip_safe=True,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Utilities',
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Utilities",
     ],
 )
